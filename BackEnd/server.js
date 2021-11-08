ab3f8bfbf17b4609563faab531dc7ebc643f71fb
KELAN RAFFERTY - STUDENT <G00382023@gmit.ie>
2021-11-08 14:39:31 +0000

    lab 6

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -19,53 +19,71 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+//database link
+connectionStr = "mongodb+srv://admin:admin@cluster0.hykwn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+// getting-started.js
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+//async connection method
+async function main() {
+  await mongoose.connect(connectionStr);
+}
+
+//schema for database
+const movieSchema = new mongoose.Schema({
+Title: String,
+Year: String,
+Poster: String
+});
+
+const movieModel = mongoose.model('movie', movieSchema);
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+//create method
 app.post('/api/movies', (req,res)=>{
     console.log(req.body);
     console.log(req.body.Title);
     console.log(req.body.Year);
     console.log(req.body.Poster);
+
+    movieModel.create({
+        Title: req.body.Title,
+        Year: req.body.Year,
+        Poster: req.body.Poster
+    })
     res.send('Data Sent to Server!')
 })
 
-app.get('/api/movies', (req, res) => {
-    const movies = [
-        {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-        },
-        {
-            "Title": "World War Z",
-            "Year": "2013",
-            "imdbID": "tt0816711",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-        }, 
-        {
-            "Title": "War of the Worlds",
-            "Year": "2005",
-            "imdbID": "tt0407304",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BNDUyODAzNDI1Nl5BMl5BanBnXkFtZTcwMDA2NDAzMw@@._V1_SX300.jpg"
-        }
-    ];
-    res.json({
-        mymovies:movies,
-        'Message':'Hello from the server'
+//find method
+app.get('/api/movies:id',(req, res) =>{
+    console.log(req.params.id);
+
+    movieModel.findById(req.params.id,(err, data) =>{
+        res.json(data);
     })
+})
+
+
+
+
+app.get('/api/movies', (req, res) => {
+    const movies = []
+        
+        
+           // "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+       
+            //"Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+       
+   movieModel.find((err, data) =>{
+        res.json(data);
+   })
+            
 })
 
 app.listen(port, () => {
